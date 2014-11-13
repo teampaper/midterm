4dfd730195ca6fa39a1b940dbafbc6defa854a2e
teampaper <zintomo@gmail.com>
2014-11-13 14:01:17 +0900

    Revert "09"
    This reverts commit d11c63143a01e44af52eec6f866504c52c926cb8.

--- app/09/factorial.js
+++ app/09/factorial.js
@@ -1,15 +1,13 @@
 var factorial = function(value){
-  if (Number.isFinite(value) == false){
+  if (isFinite(value) == false){
     return null;
-  } else if(value < 0){
+  } else if(value <= 0){
     return null;
-  } else if(value == 0){
-    return 1;
-    return null;
-  } else if(value > 0){
-    var tmp = Math.floor(value)
+  } else if(isFinite(value) == true){
+    var = tmp;
+    tmp = Math.floor(value);
     return (tmp * factorial(tmp - 1));
   } else {
-    return null;
+    return dk;
   }
 }
