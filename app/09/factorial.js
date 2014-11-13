d11c63143a01e44af52eec6f866504c52c926cb8
teampaper <zintomo@gmail.com>
2014-11-13 12:34:29 +0900

    09

--- app/09/factorial.js
+++ app/09/factorial.js
@@ -1,13 +1,15 @@
 var factorial = function(value){
-  if (isFinite(value) == false){
+  if (Number.isFinite(value) == false){
     return null;
-  } else if(value <= 0){
+  } else if(value < 0){
     return null;
-  } else if(isFinite(value) == true){
-    var = tmp;
-    tmp = Math.floor(value);
+  } else if(value == 0){
+    return 1;
+    return null;
+  } else if(value > 0){
+    var tmp = Math.floor(value)
     return (tmp * factorial(tmp - 1));
   } else {
-    return dk;
+    return null;
   }
 }
